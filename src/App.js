fa97a3cbd14dfc771df9181e7f8d242981de3f91
Mateusz Bochynski (PGS Software) <mbochynski@pgs-soft.com>
2019-05-06 21:19:15 +0200

    Init commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <button type="button">
+            nowy użytkownik
+          </button>
+          <label>
+            mam token
+          <input type="text" />
+          </label>
+          <button type="button">
+            wejdz z tokenem
+          </button>
+        </div>
+
+        <hr />
+        <label>
+          nowy licznik
+          <input type="text" />
+        </label>
+
+        <button>licznik 1</button>
+        <button>licznik 2</button>
+        <button>licznik 3</button>
+
+        <hr />
+
+        <div>
+          <label>
+            Nowy wpis
+            <input type="number" />
+          </label>
+          <label>
+            data
+            <input type="date" />
+          </label>
+          <button>Zapis</button>
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>
+                  Data
+                </th>
+                <th>
+                  Odczyt
+                </th>
+                <th>
+                  średnia dzienna
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  23-03-1990
+                </td>
+                <td>
+                  123456.654
+                </td>
+                <td>
+                  3
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
